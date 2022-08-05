0a32e9bb9fc9a6126aacfb4e102779949f4eb46a
Jungwon  Kim <86811808+kimwonny8@users.noreply.github.com>
2022-08-06 00:09:12 +0900

    [Silver V] Title: 막대기, Time: 128 ms, Memory: 12860 KB -BaekjoonHub

---
+++ 백준/Silver/1094. 막대기/막대기.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int x=sc.nextInt();
+		int cnt=0, stick=64;
+		while(x>0) {
+			if(stick>x) stick/=2; 
+			else {
+				cnt++; x-=stick;
+			}
+		}
+		System.out.println(cnt);
+	}
+}
