3f5b6501dac36f4eac7f66ec40f81c969c192bcc
Jungwon  Kim <86811808+kimwonny8@users.noreply.github.com>
2022-08-20 03:10:42 +0900

    코드 재정렬

--- 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.java
+++ 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.java
@@ -3,31 +3,31 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		 Scanner sc = new Scanner(System.in);
-		 StringBuilder sb = new StringBuilder();
-		 
-		 // 최소힙(올라갈수록 값이 작아지는 구조)과 최대힙(올라갈수록 값이 커지는 구조) 이용
-		 // 가운데에 있는 숫자는 최대힙의 root값
-	     PriorityQueue<Integer> min = new PriorityQueue<>();
-	     PriorityQueue<Integer> max = new PriorityQueue<>((o1,o2) -> o2-o1);     
-	 
-	     int N=sc.nextInt();
-	     
-	     while(N-->0) {
-	    	 int num=sc.nextInt();
-	    	 
-	    	 // max부터 추가
-	    	 if(min.size()==max.size()) max.add(num);
-	    	 else min.add(num);
+		Scanner sc=new Scanner(System.in);
+		StringBuilder sb=new StringBuilder();
 
-	    	 if(!min.isEmpty() && !max.isEmpty() && max.peek() > min.peek()) { // max의 peek이 더 크면 swap
-	                int tmp=min.poll();
-	                min.add(max.poll());
-	                max.add(tmp);
-	            }
-	            sb.append(max.peek()+"\n");        
-	    }
+		// 최소힙(올라갈수록 값이 작아지는 구조)과 최대힙(올라갈수록 값이 커지는 구조) 이용
+		// 가운데에 있는 숫자는 최대힙의 root값
+		PriorityQueue<Integer> min=new PriorityQueue<>();
+		PriorityQueue<Integer> max=new PriorityQueue<>((o1, o2) -> o2 - o1);
 
-	     System.out.println(sb);
-	    }
+		int N=sc.nextInt();
+
+		while(N-->0) {
+			int num=sc.nextInt();
+
+			// max부터 추가
+			if(min.size()==max.size()) max.add(num);
+			else min.add(num);
+
+			if(!min.isEmpty() && !max.isEmpty() && max.peek()>min.peek()) { // max의 peek이 더 크면 swap
+				int tmp=min.poll();
+				min.add(max.poll());
+				max.add(tmp);
+			}
+			sb.append(max.peek() + "\n");
+		}
+
+		System.out.println(sb);
 	}
+}
