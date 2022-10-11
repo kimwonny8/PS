5fac656b4d4a4a96a21d7d66aa3a6df20114a3c9
Jungwon  Kim <86811808+kimwonny8@users.noreply.github.com>
2022-10-11 12:31:02 +0900

    [Silver IV] Title: 수 이어 쓰기 1, Time: 468 ms, Memory: 11800 KB -BaekjoonHub

---
+++ 백준/Silver/1748. 수 이어 쓰기 1/수 이어 쓰기 1.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int cnt = 0;
+		int plus = 1;	
+		int num = 10;
+		int N = Integer.parseInt(br.readLine());
+		for(int i=1; i<=N; i++) {
+			if(i%num == 0) {
+				plus ++;
+				num *= 10;
+			}
+			cnt += plus;
+
+		}
+		System.out.println(cnt);
+	}
+
+}
