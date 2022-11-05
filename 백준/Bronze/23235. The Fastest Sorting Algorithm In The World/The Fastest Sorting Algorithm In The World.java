7b9d3d09b62e8d1f1d849c3c067a75c95724c41f
Jungwon  Kim <86811808+kimwonny8@users.noreply.github.com>
2022-11-05 20:58:34 +0900

    [Bronze V] Title: The Fastest Sorting Algorithm In The World, Time: 224 ms, Memory: 21092 KB -BaekjoonHub

---
+++ 백준/Bronze/23235. The Fastest Sorting Algorithm In The World/The Fastest Sorting Algorithm In The World.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int cnt = 0;
+        while (true) {
+            int N = sc.nextInt();
+
+            if (N == 0) {
+                break;
+            }
+            for (int i =0 ; i <N; i++) {
+                int M = sc.nextInt();
+            }
+
+            cnt++;
+
+            System.out.println("Case " +cnt+": Sorting... done!");
+        }
+
+    }
+}
