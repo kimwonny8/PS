df74e0dd8a218301a87c6e8cf46aafae031246e1
Jungwon  Kim <86811808+kimwonny8@users.noreply.github.com>
2022-11-23 20:44:38 +0900

    [Bronze II] Title: 가장 많은 글자, Time: 112 ms, Memory: 12948 KB -BaekjoonHub

---
+++ 백준/Bronze/1371. 가장 많은 글자/가장 많은 글자.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int[] arr = new int[26];
+
+		while (sc.hasNextLine()) {
+			String str = sc.nextLine();
+			for (int i = 0; i < str.length(); i++) {
+				if (str.charAt(i) >= 'a' && str.charAt(i) <= 'z')
+					arr[str.charAt(i) - 'a']++;
+			}
+		}
+
+		int max = 0;
+		for (int i = 0; i < 26; i++) {
+			if (max < arr[i]) max = arr[i];
+		}
+
+		for (int i = 0; i < 26; i++) {
+			if (max == arr[i]) System.out.print((char) (i + 'a'));
+		}
+	}
+}
