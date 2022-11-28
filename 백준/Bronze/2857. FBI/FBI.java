d53b0b2456ae05b57871c592991e4ccb3457090f
Jungwon  Kim <86811808+kimwonny8@users.noreply.github.com>
2022-11-28 21:19:22 +0900

    [Bronze III] Title: FBI, Time: 108 ms, Memory: 12836 KB -BaekjoonHub

---
+++ 백준/Bronze/2857. FBI/FBI.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main{
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        String [] arr = new String[5];
+        int cnt = 0;
+        for(int i=0; i<arr.length; i++) arr[i] = sc.next();
+      
+        for(int i=0; i<arr.length; i++){
+        	if(arr[i].contains("FBI")) {
+        		System.out.print(i+1+" ");
+        		cnt++;
+        	}
+        }
+        if(cnt == 0)
+        	System.out.print("HE GOT AWAY!");
+    }
+}
